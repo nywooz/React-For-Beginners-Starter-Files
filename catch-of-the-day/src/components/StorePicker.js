c6b66e60b4f394a0b8ab6069773c8364cbd9c719
nywooz8 <nywooz8@gmail.com>
2018-10-30 12:40:21 +0000

    tut handling events/router history

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,13 +2,31 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-  render() {
+  myInput = React.createRef();
+
+  goToStore = e => {
+    // 1. stop default form submit
+    e.preventDefault();
+    // 2. get text from that input
+    const storeName = this.myInput.value.value;
+
+    // 3. change the page to /store/whateva-they-entered
+    this.props.history.push(`/store/${storeName}`);
     console.log(this);
+  };
+
+  componentDidMount() {
+    console.log(this);
+  }
+
+  render() {
     return (
       <React.Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a store</h2>
+
           <input
+            ref={this.myInput}
             type="text"
             required
             placeholder="Store Name"
